9d59a552d540cfdaaa9eb2fe9f493f26a134cd08
Ben Holleran <wilddoogyplumb@gmail.com>
2023-03-21 19:15:37 -0400

    Setting up Python.

--- ESP8266/Fireflies/server.py
+++ ESP8266/Fireflies/server.py
@@ -1,4 +1,4 @@
-import http
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 # a server that serves index.html
@@ -6,6 +6,26 @@ def index(req):
     with open("index.html", "r") as f:
         return f.read()
 
+class MyHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(index(self).encode())
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+
+def run(server_class=HTTPServer, handler_class=MyHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
 
 if __name__ == "__main__":
-    http.start_server(index)
+    print("Starting server at http://localhost:8000")
+    run()
